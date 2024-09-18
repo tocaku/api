4e6388e5ab0d9e382eda0e956daab0406161dda2
tocaku <bogiveler@gmail.com>
2024-09-18 22:31:38 +0300

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const SITE_URL = "https://f1connectapi.vercel.app/";
+export const SITE_URL_API = "https://f1connectapi.vercel.app/api/";
+export const SITE_NAME = "F1  tocaku";
+export const SITE_TITLE = "F1 tocaku Api.";
+export const SITE_META_NAME =
+  "F1 tocaku API | Your free API ready for development";
+export const SITE_META_DESCRIPTION =
+  "F1 tocaku API it's a free API that provides data about all Formula 1 Seasons.";
